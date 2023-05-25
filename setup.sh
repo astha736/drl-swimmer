4fbc54f7d1a77150f7933dbd950c98fb7dce7c96
Oliver Hausdoerfer <hausdoer@biorobcn1-dev>
2023-05-25 10:35:24 +0200

    fix setups; add farms file for video

--- setup.sh
+++ setup.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 cd farms
 cd farms_core
-pip3 install -r requirements.txt
-pip3 install .
+pip3 install -r requirements.txt --no-cache-dir
+pip3 install . --no-cache-dir
 cd ..
 cd farms_mujoco
-pip3 install -r requirements.txt
-pip3 install .
+pip3 install -r requirements.txt --no-cache-dir
+pip3 install . --no-cache-dir
 cd ..
 cd farms_sim
-pip3 install -r requirements.txt
-pip3 install .
+pip3 install -r requirements.txt --no-cache-dir
+pip3 install . --no-cache-dir
 cd ..
 cd farms_amphibious
-pip3 install -r requirements.txt
-pip3 install .
+pip3 install -r requirements.txt --no-cache-dir
+pip3 install . --no-cache-dir
 cd ..
-pip3 install -e farms_core
-pip3 install -e farms_mujoco
-pip3 install -e farms_sim
-pip3 install -e farms_amphibious
+pip3 install -e farms_core --no-cache-dir
+pip3 install -e farms_mujoco --no-cache-dir
+pip3 install -e farms_sim --no-cache-dir
+pip3 install -e farms_amphibious --no-cache-dir
 cd ..
-pip3 install .
+pip3 install . --no-cache-dir
+pip install sb3-contrib stable-baselines3 tensorboard black ffmpeg gym --no-cache-dir
