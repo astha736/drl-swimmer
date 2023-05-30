c89497b0fa23783c469476458601b60c9a78e97a
Oliver Hausdoerfer <hausdoer@biorobpclab1.epfl.ch>
2023-05-30 07:38:31 +0200

    fix reset oscillators

--- agnathax_control/network.py
+++ agnathax_control/network.py
@@ -22,11 +22,11 @@ class AnimatNetwork(ABC):
 
     @abstractmethod
     def step(
-            self,
-            iteration: int,
-            time: float,
-            timestep: float,
-            **kwargs,
+        self,
+        iteration: int,
+        time: float,
+        timestep: float,
+        **kwargs,
     ):
         """Step function called at each simulation iteration"""
 
@@ -36,43 +36,43 @@ class NetworkODETEST(AnimatNetwork):
 
     def __init__(self, data, **kwargs):
         state_array = data.state.array
+        self.state_aray_init = data.state.array
         super().__init__(data=data, n_iterations=np.shape(state_array)[0])
         self.dstate = np.zeros_like(data.state.array[0, :])
-        self.ode: Callable = kwargs.pop('ode', ode_oscillators_sparse)
+        self.ode: Callable = kwargs.pop("ode", ode_oscillators_sparse)
         self.solver: ODE = integrate.ode(f=self.ode)
-        self.solver.set_integrator('dopri5', **kwargs)
+        self.solver.set_integrator("dopri5", **kwargs)
         self.solver.set_initial_value(y=state_array[0, :], t=0.0)
 
     def copy_next_drive(self, iteration):
         """Set initial drive"""
         array = self.data.network.drives.array
-        array[iteration+1] = array[iteration]
+        array[iteration + 1] = array[iteration]
 
     def step(
-            self,
-            iteration: int,
-            time: float,
-            timestep: float,
-            checks: bool = False,
+        self,
+        iteration: int,
+        time: float,
+        timestep: float,
+        checks: bool = False,
     ):
         """Control step"""
         if iteration == 0:
+            self.dstate = np.zeros_like(self.state_aray_init[0, :])
+            self.solver.set_initial_value(y=self.state_aray_init[0, :], t=0.0)
             self.copy_next_drive(iteration)
             return
         if checks:
-            assert np.array_equal(
-                self.solver.y,
-                self.data.state.array[iteration, :]
-            )
+            assert np.array_equal(self.solver.y, self.data.state.array[iteration, :])
         self.solver.set_f_params(self.dstate, iteration, self.data)
-        self.data.state.array[iteration, :] = (
-            self.solver.integrate(time+timestep, step=True)
+        self.data.state.array[iteration, :] = self.solver.integrate(
+            time + timestep, step=True
         )
-        if iteration < self.n_iterations-1:
+        if iteration < self.n_iterations - 1:
             self.copy_next_drive(iteration)
         if checks:
             assert self.solver.successful()
-            assert abs(time+timestep-self.solver.t) < 1e-6*timestep, (
-                'ODE solver time: '
-                f'{self.solver.t} [s] != Simulation time: {time+timestep} [s]'
+            assert abs(time + timestep - self.solver.t) < 1e-6 * timestep, (
+                "ODE solver time: "
+                f"{self.solver.t} [s] != Simulation time: {time+timestep} [s]"
             )
