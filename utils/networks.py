faae51893f68c1261a186d7882c1654f7e55e5ee
oliver <oliver@oliver.hp>
2023-06-12 19:39:04 +0200

    add non-shared net; add experiments;

--- utils/networks.py
+++ utils/networks.py
@@ -145,6 +145,90 @@ class localFeedbackShared(nn.Module):
         return self.value_net(features)
 
 
+# https://stable-baselines3.readthedocs.io/en/master/guide/custom_policy.html
+class localFeedbackNonShared(nn.Module):
+    """
+    Custom network for policy and value function.
+    It receives as input the features extracted by the features extractor.
+
+    :param feature_dim: dimension of the features extracted with the features_extractor (e.g. features from a CNN)
+    :param last_layer_dim_pi: (int) number of units for the last layer of the policy network
+    :param last_layer_dim_vf: (int) number of units for the last layer of the value network
+    """
+
+    def __init__(
+        self,
+        feature_dim: int,
+    ):
+        super().__init__()
+
+        self.n_calls: int = 0
+        self.obs_per_iter: int = 2
+
+        # IMPORTANT:
+        # Save output dimensions, used to create the distributions
+        self.latent_dim_pi = conf.CONF["RL"]["policy_network"]["arch"][1]
+        self.latent_dim_vf = conf.CONF["RL"]["policy_network"]["arch"][1]
+
+        def get_policy_net():
+            return nn.Sequential(
+                nn.Linear(
+                    self.obs_per_iter, conf.CONF["RL"]["policy_network"]["arch"][0]
+                ),
+                getattr(torch.nn, conf.CONF["RL"]["policy_network"]["act_fn"])(),
+                nn.Linear(
+                    conf.CONF["RL"]["policy_network"]["arch"][0], self.latent_dim_pi
+                ),
+                getattr(torch.nn, conf.CONF["RL"]["policy_network"]["act_fn"])(),
+            )
+
+        self.policy_nets = [get_policy_net() for i in range(10)]
+
+        # Value network
+        self.value_net = nn.Sequential(
+            nn.Linear(feature_dim, conf.CONF["RL"]["policy_network"]["arch"][0]),
+            getattr(torch.nn, conf.CONF["RL"]["policy_network"]["act_fn"])(),
+            nn.Linear(conf.CONF["RL"]["policy_network"]["arch"][0], self.latent_dim_vf),
+            getattr(torch.nn, conf.CONF["RL"]["policy_network"]["act_fn"])(),
+        )
+
+    def forward(self, features: th.Tensor) -> Tuple[th.Tensor, th.Tensor]:
+        """
+        :return: (th.Tensor, th.Tensor) latent_policy, latent_value of the specified network.
+            If all layers are shared, then ``latent_policy == latent_value``
+
+        Customized for very local feedback and shared net
+        """
+        return self.forward_actor(features), self.forward_critic(features)
+
+    def forward_actor(self, features: th.Tensor) -> th.Tensor:
+        # extract relevant obs
+        # 0-9: joint positions
+        # 10-19: phases
+        obs = th.tensor(
+            [
+                th.Tensor([features[0][self.n_calls]]),
+                th.Tensor(
+                    [features[0][self.n_calls + 1 + 10]]
+                ),  # + 1 because head phase not relevant
+            ],
+            device="cuda:0" if features.get_device() == 0 else "cpu",
+        )
+
+        out = self.policy_nets[self.n_calls](obs)
+
+        # increment call counter and reset when = 10
+        # don't try to make this as one-liner ;)
+        self.n_calls += 1
+        if self.n_calls == 9:
+            self.n_calls = 0
+
+        return out
+
+    def forward_critic(self, features: th.Tensor) -> th.Tensor:
+        return self.value_net(features)
+
+
 class CustomActorCriticPolicy(ActorCriticPolicy):
     def __init__(
         self,
@@ -171,6 +255,6 @@ class CustomActorCriticPolicy(ActorCriticPolicy):
             if conf.CONF["RL"]["localFeedback"] == "shared":
                 self.mlp_extractor = localFeedbackShared(self.features_dim)
             elif conf.CONF["RL"]["localFeedback"] == "non-shared":
-                raise NotImplementedError
+                self.mlp_extractor = localFeedbackNonShared(self.features_dim)
         else:
             self.mlp_extractor = CustomNetwork(self.features_dim)
